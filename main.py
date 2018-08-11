3b043279771ef373f1c86f466a4758af84c283e2
Arthur Manasyan <amanasyan@macadamian.com>
2018-08-11 18:46:39 +0400

    renamed "sentence" to "string"

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ pattern = r'\w+.+'    # match a line without pass/fail icons
 with open(filename) as f:
     for line in f.readlines():
         line_without_icons = re.search(pattern, line).group()
-        sentence = re.sub(r'\(\w+\)', '', line_without_icons.rstrip())  # remove (2 ms)
-        if sentence.startswith('when'):
-            sentence = '\n' + sentence + '\n'
-        print(sentence)
+        string = re.sub(r'\(\w+\)', '', line_without_icons.rstrip())  # remove (2 ms)
+        if string.startswith('when'):
+            string = '\n' + string + '\n'
+        print(string)
